58148704fc2ec1fe798e6a7b458c9a1e0004ba54
Fredrik Fastén <fredrikfasten@fredriks-mbp.lan>
2024-08-10 11:57:27 +0200

    Dynamic env variables

--- config/master.php
+++ config/master.php
@@ -7,8 +7,14 @@ foreach (glob(__DIR__ . '/../include/classes/*.php') as $filename) {
   require_once $filename;
 }
 
-$GLOBALS['protocol'] = 'http';
-$GLOBALS['domain'] = 'localhost:8080';
+// Determine the protocol (http or https) based on the environment
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? 'https' : 'http';
+
+// Determine the domain based on the environment
+$domain = $_SERVER['HTTP_HOST'] ?? 'localhost:8080';
+
+$GLOBALS['protocol'] = $protocol;
+$GLOBALS['domain'] = $domain;
 $GLOBALS['language'] = 'sv';
 $GLOBALS['supported_languages'] = [
   'en' => 'English',
